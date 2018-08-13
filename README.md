13d47e999d9ef0133109b94d0f4c6f702e951de5
Robert Kowalski <rok@kowalski.gd>
2018-08-13 18:47:43 +0200

    init - complete exchange with hive adapter

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# cheesebox
+# Cheesebox
+
+Cheesebox is a minimalistic UI to build own exchanges. It's build with React and Redux. The Redux side is kept simple, so its easy to extend for your own use cases.
+
+All API calls are abstracted by a client library taking care of the connection. This means Cheesebox accepts a client which does the data conversions and maps the corresponding endpoints.
+
+So far this repository contains examples for eos/eosfinex (`index-eos.js`) and hive (index-hive.js).
+
+If you created an own exchange / connector, feel free to submit a [Pull Request]().
+
+
+```
+# eosfinex
+
+nodemon -e js,jsx dev-eos.js -w src
+
+# hive
+
+nodemon -e js,jsx dev-hive.js -w src
+```
+
+
+## Adding your own exchange
+
+1. Implement your own client, see `src/adapters` for examples. Extend the exisiting methods of the base Mandelbrot class.
+2. Modify `src/app-hive.jsx` to load your own client and user config
+3. Profit!
---
+++ src/containers/positions.jsx
@@ -0,0 +1,140 @@
+'use strict'
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import OrderbookRow from '../common/book-positions-row.jsx'
+import ErrorBox from '../common/errorbox.jsx'
+
+import {
+  setPositions
+} from '../actions.js'
+
+class PositionsContainer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.client = this.props.client
+  }
+
+  updateInterval (pair, user) {
+    const { dispatch } = this.props
+
+    if (this.periodicFetch) {
+      clearInterval(this.periodicFetch)
+    }
+
+    this.periodicFetch = setInterval(() => {
+      this.client.orders({ pair: pair, user: user })
+        .then((res) => {
+          const pl = { asks: res.asks, bids: res.bids, error: null }
+          dispatch(setPositions(pl))
+        })
+        .catch((err) => {
+          dispatch(setPositions({ asks: [], bids: [], error: err }))
+        })
+    }, 1000)
+  }
+
+  componentWillReceiveProps (props) {
+    const { user, pair } = props
+
+    if (props.pair === this.props.pair) {
+      return false
+    }
+
+    this.updateInterval(pair, user)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.periodicFetch)
+  }
+
+  cancelOrder (data) {
+    this.client.cancel(data)
+  }
+
+  render () {
+    const {
+      bidsPositions = [],
+      asksPositions = [],
+      errorPositions = null,
+      pair = ''
+    } = this.props
+
+    return (
+      <PositionsInternal
+        pair={pair}
+        bids={bidsPositions}
+        asks={asksPositions}
+        error={errorPositions}
+        cancelcb={(data) => {
+          this.cancelOrder(data)
+        }}
+      />
+    )
+  }
+}
+
+class PositionsInternal extends Component {
+  render () {
+    const {
+      bids = [],
+      asks = [],
+      error = false,
+      cancelcb = () => console.log('cancelcb called for positions'),
+      pair
+    } = this.props
+
+    if (error) {
+      return <ErrorBox error={error.message} />
+    }
+
+    return (
+      <div className='column column-65 column-offset-10 positions__internal'>
+        <div className='positions__title'>
+          Positions for {pair}
+        </div>
+        <div className='orderbook__explaintable rowI'>
+          <div className='row__price'>
+            Price
+          </div>
+          <div className='row__quantity'>
+            Quantity
+          </div>
+          <div className='row__cancelbutton' />
+        </div>
+        <div className='positions__bids'>
+          {
+            bids.map((bidRow, i) => {
+              return <OrderbookRow key={i} data={bidRow} cancelcb={cancelcb} pair={pair} side='bids' />
+            })
+          }
+        </div>
+        <div className='positions__asks'>
+          {
+            asks.map((askRow, i) => {
+              return <OrderbookRow key={i} data={askRow} cancelcb={cancelcb} pair={pair} side='asks' />
+            })
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  const {
+    pairPairs,
+    user,
+    positions
+  } = state
+
+  return {
+    ...pairPairs,
+    ...user,
+    ...positions
+  }
+}
+
+export default connect(mapStateToProps)(PositionsContainer)
